4040a7cf449e75e07e025c489520148e7b0f5761
Pedro A. <pedroaugusto732@gmail.com>
2025-09-09 15:55:32 -0300

    Criação de API RESTful de enquentes -Opinas

---
+++ src/main/java/com/PedroA10/Opinas/OpinasApplication.java
@@ -0,0 +1,13 @@
+package com.PedroA10.Opinas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpinasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpinasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/PedroA10/Opinas/OpinasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.PedroA10.Opinas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpinasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
